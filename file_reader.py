a99047b209438a0c49d43f87668f4419e02fd149
Binh Nguyen <BinhNguyenAn@0587404233.wireless.umich.net>
2019-12-17 16:12:54 -0500

    filereader

---
+++ file_reader.py
@@ -0,0 +1,17 @@
+import os
+import plotly.express as px
+
+def file_reader(filename):
+
+    # hover to show headline + abstract
+    list_1 = []
+    list_2 = []
+    full_path = os.path.join(os.path.dirname(__file__), filename)
+    f = open(full_path, "r")
+    lines = f.readlines()
+    for line in lines[1:]:
+        line = line.split(",")
+        list_1.append(int(line[0]))
+        list_2.append(float(line[1].strip()))
+
+    return (list_1, list_2)
